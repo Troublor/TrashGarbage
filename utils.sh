67e04f52d4bd6c448830cde39e1d91096d5f9856
troublor <troublor@live.com>
2019-04-17 10:23:33 +0800

    add option -e to find_item

--- utils.sh
+++ utils.sh
@@ -5,7 +5,15 @@ source $HOME/bin/scripts/trash/trash.conf
 find_item(){
     # find item in trash data file
     # return a list of line number (seperated by space): the line number of the record, -1 if it doesn't exist
-    reg="^[F|D]\ +.+\ +$1.*\ *$"
+    if [ $1 == "-e" ]
+    then 
+        # precisely match
+        reg="^[F|D]\ +.+\ +$2\ *$"
+    else 
+        # prefix match
+        reg="^[F|D]\ +.+\ +$1.*\ *$"
+    fi
+    
     raw=$(cat $TRASH_DATA_FILE | grep -n -E "$reg")
     OLD_IFS="$IFS"
     IFS=$'\n'
