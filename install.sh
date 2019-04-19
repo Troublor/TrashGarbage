80c8d8335003bba57fd6552c4cb16320d7af94a3
troublor <troublor@live.com>
2019-04-19 16:10:58 +0800

    make some changes to log info

--- install.sh
+++ install.sh
@@ -6,22 +6,22 @@ TRASH_ROOT_PATH=$(cd "$(dirname $0)"; pwd)
 source $TRASH_ROOT_PATH/trash.conf
 
 
-echo "[INFO] Create Trash Garbage Folder $TRASH_PATH"
+echo "[INFO] Create Recycle Bin Folder $TRASH_PATH"
 if [ -e $TRASH_PATH ]
 then
-    echo -n "[WARN] Trash Garbage Folder $TRASH_PATH already exists. Are you sure to override it? (y/n) "
+    echo -n "[WARN] Recycle Bin Folder $TRASH_PATH already exists. Are you sure to override it? (y/n) "
     read input
     if [ "$input" == "Y" -o "$input" == "y" ]
     then
         rm -d -r $TRASH_PATH
-        echo "[INFO] Delete Trash Garbage Folder $TRASH_PATH"
+        echo "[INFO] Delete Recycle Bin Folder $TRASH_PATH"
     else
         echo "[INFO] Abort installation"
         exit 0
     fi
 fi
 
-echo "[INFO] Create Trash Garbage Folder $TRASH_PATH"
+echo "[INFO] Create Recycle Bin Folder $TRASH_PATH"
 mkdir -p $TRASH_PATH
 
 BIN_PATH=$TRASH_ROOT_PATH/bin
